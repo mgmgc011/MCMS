bffc8393ce315a962ad3805d89c3a77364bdf52e
Mingu Chu <mingu@mingus-macbook-pro.local>
2016-03-22 14:09:50 -0500

    Initial Commit

---
+++ MCMS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MCMS
+//
+//  Created by Mingu Chu on 3/22/16.
+//  Copyright © 2016 Mingu Chu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
